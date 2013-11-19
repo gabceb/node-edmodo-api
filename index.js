1c2d2484b7c40050a867f97768c5fb7ee0269efb
Gabriel Cebrian <gabceb@gmail.com>
2013-11-19 12:08:58 -0800

    Moved array helper function to its own helper file

--- index.js
+++ index.js
@@ -4,34 +4,8 @@ events = require('events'),
 _und = require('underscore'),
 _und_s = require('underscore.string'),
 moment = require('moment'),
+helper = require('./lib/helper'),
 config = require('./config.js')();
-
-// Helper functions
-Array.prototype.to_params = function(){
-	var params = '[';
-
-	for(var i = 0; i < this.length; i++)
-	{
-		// TO DO: Refactor this method
-		if (typeof this[i] === 'string') {
-          params += '"' + this[i] + '"';
-      }
-      else if(typeof this[i] == 'object'){
-         params += JSON.stringify(this[i]);
-     }
-     else {
-         params += this[i];
-     }
-
-
-     if(i != this.length - 1)
-     {
-         params += ",";
-     }
- }
-
- return params + "]";
-};
 
 var _my = null;
 
